ab4230d1bcf425d4bab35595492f5d3d79deeb9b
Taylor E Berger <taylor@taylor-ultra.(none)>
2013-11-21 15:45:58 -0700

    fleshed out abstract implementation for gng.py. TODO: Error functions, statistic generators, knitting it together

--- gngClusterer/gng.py
+++ gngClusterer/gng.py
@@ -1,38 +1,50 @@
 import networkx as nx
 import random
+from functools import partial
 
 class gng(networkx):
-    def __init__(self, error_fn, point_x = [0], point_y = [0]):
+    def __init__(self, error_fn, node_gen, learning_rate):
+        self.node_generator = node_gen
         self.err_fn = error_fn
-        self.network = nx.Graph(point_x, point_y)
-        self.network.add_edge(point_x, point_y)
+        self.learning_rate = learning_rate
 
-    def grow(self, backing_graph):
+        #initialize the network to two points
+        rand_point_x = node_gen.generate(mode = "random")
+        rand_point_x = node_gen.generate(mode = "random")
+        self.network = nx.Graph()
+        self.network.add_node(rand_point_x)
+        self.network.add_node(rand_point_y)
+        self.network.add_edge(rand_point_x, rand_point_y)
+
+    def grow(self):
         #find node with highest error -- distance to closest node
-        err_node = max_args(error_fn, self.network.nodes())
+        err_node = self.error_fn(self.network.nodes(), mode="max")
 
         #find connected node with highest error
-        err_neighbor = max_args(error_fn, self.network.get_edges(err_node))
+        err_neighbor = self.error_fn(self.network.edge(err_node), mode="max")
 
         #delete edge between the two
         self.network.remove_edge(err_node, err_neighbor)
 
         #insert new node
-        new_node = pass
+        new_node = self.node_generator.generate(mode = "average", x = err_node, y = err_neighbor)
+
         #connect to other two nodes with highest error
         self.network.add_edge(err_node, new_node)
         self.network.add_edge(err_neighbor, new_node)
 
     def fit(self, point):
+        #search for nearest neighbors        
+        #dynamic neighborhood size of 5% of the network
+        neighborhood_size = int(self.network.node() * 0.05)
+
         #stimulate graph using underlying data -- point param
-        #search for nearest neighbors
+        best_nodes = self.error_fn(self.network.nodes(), mode="min", size = neighborhood_size)
+
         #pull nodes in direction of stimulation
-        pass
-
-
-def max_args(fn, *ls, curr = null):
-    if fn(ls[0]) > curr:
-        return max_args(fn, ls[1:], ls[0])
-    else:
-        return max_args(fn, ls[1:], curr)
-    
+        diminishing_return = 0.9
+        alpha = self.learning_rate
+        for node in best_nodes:
+            new_node = self.node_generator.generate(mode = "average", x = node, y = point, x_weight = 1-alpha, y_weight = alpha)
+            alpha = alpha * diminishing_return
+            self.network[node] = new_node
